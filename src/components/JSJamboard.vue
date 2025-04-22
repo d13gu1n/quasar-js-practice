112d6500a50bbe585e4721178e845b3b3609cb03
Dieguin <d.acevedom1993@gmail.com>
2025-04-21 20:47:05 -0400

    First practice commit

---
+++ src/components/JSJamboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{ passedTestLabel }}</div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue"
+
+export default defineComponent({
+  name: "JSJamboard",
+  data() {
+    return {
+      ref
+    }
+  },
+  props: {
+    passedTestLabel: String
+  }
+})
+
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Ambiente de Quasar </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <q-btn unelevated dense label="Probá" no-caps color="primary" @click="testMethod" class="q-py-sm" />
+      <JSJamboard :passedTestLabel="testLabel"/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import JSJamboard from 'components/JSJamboard.vue'
+let testLabel
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    JSJamboard,
+  },
+  data() {
+    return {
+      ref,
+      testLabel,
+    }
+  },
+
+  methods: {
+    // TODO: Practice methods and algorithms here
+    testMethod() {
+      console.log('It works!! 💓')
+      this.testLabel = 'It is, indeed, working!! 💓'
+    },
+  },
+})
+</script>
